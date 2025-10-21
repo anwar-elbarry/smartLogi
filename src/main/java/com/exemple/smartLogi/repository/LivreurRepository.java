289ffe5424cf166cc81410592a4d8a1bdeb14a38
anouar eil barry <elbarryanwar37@gmail.com>
2025-10-21 15:13:43 +0100

    feat:cerate repositories(colis / livreur)

--- src/main/java/com/exemple/smartLogi/repository/LivreurRepository.java
+++ src/main/java/com/exemple/smartLogi/repository/LivreurRepository.java
@@ -1,4 +1,10 @@
 package com.exemple.smartLogi.repository;
 
-public class LivreurRepository {
+import com.exemple.smartLogi.model.Livreur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivreurRepository extends JpaRepository<Livreur, String> {
+
 }
