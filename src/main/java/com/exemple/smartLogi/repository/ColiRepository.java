0e73c03695988c09beb061649126a66c2f5783c0
anouar eil barry <elbarryanwar37@gmail.com>
2025-10-22 11:41:44 +0100

    fix: remove unnecessary @Repository annotations from repository interfaces

--- src/main/java/com/exemple/smartLogi/repository/ColiRepository.java
+++ src/main/java/com/exemple/smartLogi/repository/ColiRepository.java
@@ -2,8 +2,6 @@ package com.exemple.smartLogi.repository;
 
 import com.exemple.smartLogi.model.Colis;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ColiRepository extends JpaRepository<Colis, String> {
 }
--- src/main/java/com/exemple/smartLogi/repository/LivreurRepository.java
+++ src/main/java/com/exemple/smartLogi/repository/LivreurRepository.java
@@ -2,9 +2,7 @@ package com.exemple.smartLogi.repository;
 
 import com.exemple.smartLogi.model.Livreur;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface LivreurRepository extends JpaRepository<Livreur, String> {
 
 }
