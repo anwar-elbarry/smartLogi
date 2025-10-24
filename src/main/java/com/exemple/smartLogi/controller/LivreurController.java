a55e1d9b37607331d8ccca3ca35d3902927be7c9
anouar eil barry <elbarryanwar37@gmail.com>
2025-10-24 17:04:32 +0100

    feat: add LivreurController for handling delivery operations

---
+++ src/main/java/com/exemple/smartLogi/controller/LivreurController.java
@@ -0,0 +1,14 @@
+package com.exemple.smartLogi.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class LivreurController implements Controller {
+    @Override
+    public @Nullable ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+}
